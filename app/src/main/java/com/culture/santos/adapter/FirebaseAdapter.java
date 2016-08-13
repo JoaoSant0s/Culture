5cb8f1f32fb82da43aaf89cd3731e995423c6a02
Joao Ricardo <Joao Ricardo>
2016-08-13 11:47:32 -0300

    Populating the FireBaseAdapter with specifics variables.

--- app/src/main/java/com/culture/santos/adapter/FirebaseAdapter.java
+++ app/src/main/java/com/culture/santos/adapter/FirebaseAdapter.java
@@ -1,7 +1,23 @@
 package com.culture.santos.adapter;
 
+import com.culture.santos.culture.MapsActivity;
+import com.firebase.client.Firebase;
+import com.firebase.client.core.Context;
+
 /**
  * Created by Ricar on 13/08/2016.
  */
 public class FirebaseAdapter {
+
+    private Firebase fireBase;
+    private final String BACKGROUND_FIREBASE_URL = "https://culture-7b369.firebaseio.com";
+
+
+    public FirebaseAdapter(MapsActivity context){
+        Firebase.setAndroidContext(context);
+    }
+
+    public void defineFireBase(){
+        fireBase = new Firebase(BACKGROUND_FIREBASE_URL);
+    }
 }
